c012088502bba669fb71b49186aa1b42ae77e970
Ashutosh <ashutoshtiwari8172@gmail.com>
2024-04-06 16:36:05 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import localFont from 'next/font/local'
 import type { Metadata } from "next";
+import './globals.css'
 
-import "./globals.css";
+
 import Footer from "@/components/Footer";
 
 
@@ -9,6 +11,27 @@ export const metadata: Metadata = {
   title: "Maia",
   description: "Maia is a disrupting tech in passwordless authentication. Maia's novel architecture offers a seamless (stealthy) authorization experience for the users. Moreover, Maia ensures security even if user credentials are compromised; hence there are no harmful consequences of user corruption or phishing attacks.",
 };
+const PPObjectSans = localFont({
+  src: [
+    {
+      path: './assets/PPObjectSans-Regular.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './assets/PPObjectSans-Medium.woff',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './assets/PPObjectSans-Semibold.woff',
+      weight: '600',
+      style: 'normal',
+    },
+
+  ],
+})
+
 
 export default function RootLayout({
   children,
@@ -16,7 +39,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={PPObjectSans.className}>
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
